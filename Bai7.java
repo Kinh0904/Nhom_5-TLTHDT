db6b81860cdca499b170bfbf9254089b249130e0
vuthiphuongthaoyt-star <vuthiphuongthaoyt@gmail.com>
2025-09-18 14:40:42 +0700

    Add files via upload

---
+++ Bai7.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Bai7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhap so luong phan tu cua mang: ");
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Nhap cac phan tu cua mang:");
+        for (int i = 0; i < n; i++) {
+            System.out.print("arr[" + i + "] = ");
+            arr[i] = scanner.nextInt();
+        }
+        int tong = 0;
+        for (int i = 0; i < n; i++) {
+            tong += arr[i];
+        }
+        System.out.println("Tong cac phan tu trong mang = " + tong);
+
+        scanner.close();
+    }
+}
